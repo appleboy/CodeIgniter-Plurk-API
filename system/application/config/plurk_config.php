76d29629f3177ba9397894935a83efd7a8f83516
appleboy <appleboy.tw@gmail.com>
2009-12-29 15:17:59 +0800

    edit config/plurk_constant.php

--- system/application/config/plurk_config.php
+++ system/application/config/plurk_config.php
@@ -1,11 +1,8 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-	/* for path */
-	define('PLURK_COOKIE_PATH', BASEPATH . 'logs/cookie');
-	define('PLURK_LOG_PATH', BASEPATH . 'logs/plurk_log');
 
-	define('PLURK_NOT_LOGIN', 'You are not login.');
-	define('PLURK_FIELD_NOT_EMPTY', 'field can\'t be empty');
-	
-	define('PLURK_AGENT', 'php-plurk-api agent');
+$config['api_key']	= "xxxxx";
+$config['username'] = "xxxxx";
+$config['password'] = "xxxxx";
+
 ?>
