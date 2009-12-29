257d24256130546fe389b323add2204f02380c9c
appleboy <appleboy.tw@gmail.com>
2009-12-29 15:22:26 +0800

    edit config/plurk_config.php

--- system/application/config/plurk_config.php
+++ system/application/config/plurk_config.php
@@ -1,7 +1,7 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['api_key']	= "xxxxx";
+$config['api_key'] = "xxxxx";
 $config['username'] = "xxxxx";
 $config['password'] = "xxxxx";
 
